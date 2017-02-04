5d6a14bd0cb8c1ac6abc8fe4c50d4065112682ad
Varun Kudva <varun.kudva@gmail.com>
2017-02-03 16:36:04 -0800

    New problems

--- leetcode/climbSteps.py
+++ leetcode/climbSteps.py
@@ -1,5 +1,10 @@
 # climb stairs problem from leetcode
 # Backtracking solution
+# You are climbing a stair case. It takes n steps to reach to the top.
+#
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+#
+# Note: Given n will be a positive integer.
 def climb(top, ways, path):
 
     if top == 0:
