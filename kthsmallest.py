753cb01ff5545d6e74e2a8de1ecb8973145a117d
Varun Kudva <varun.kudva@gmail.com>
2017-02-13 22:09:18 -0800

    - Changes to kthSmallest - Topological sort for Graph

---
+++ kthsmallest.py
@@ -0,0 +1,48 @@
+"""
+kth smallest/largest element
+- use quickselect algorithm (part of quicksort)
+  to choose pivot element which is at index k-1
+- Can be used to find medians in unsorted arrays
+  by having k = len/2.
+"""
+def swap(arr, i, j):
+    temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+
+def partition(arr, left, right):
+    """
+    partition the array around a pivot element,
+    with elements < pivot to the left of pivot
+    and elements >= pivot to the right of pivot
+    """
+    # can randomise pivot idx
+    pivot_idx = right
+    pivot = arr[pivot_idx]
+    part_idx = left
+    swap(arr, pivot_idx, right)
+    for i in range(left, right):
+        if arr[i] < pivot:
+            swap(arr, i, part_idx)
+            part_idx += 1
+    swap(arr, part_idx, right)
+    return part_idx
+
+
+def quick_select(arr, left, right, k):
+    if left == right:
+        return arr[left]
+
+    p = partition(arr, left, right)
+    if p == k-1:
+        return arr[p]
+    elif p > k-1:
+        return quick_select(arr, left, p-1, k)
+    else:
+        return quick_select(arr, p+1, right, k)
+
+
+if __name__ == '__main__':
+    arr = [4, 9, 2, 8, 6, 5, 3]
+    print quick_select(arr, 0, len(arr)-1, 3)
+    print arr
--- lib/graph.py
+++ lib/graph.py
@@ -16,7 +16,7 @@ class Graph():
     '''
     def __init__(self, v):
         self.adj = defaultdict(list)
-        self.num_vertices = v
+        self.vertices = v
         self.num_edges = 0
 
         # marker to identify nodes visited
@@ -27,7 +27,7 @@ class Graph():
         self.count = 0
 
     def _validate_vertex(self, v):
-        if v < 0 or v >= self.num_vertices:
+        if v < 0 or v >= self.vertices:
             raise
 
     def add_edge(self, u, v):
@@ -55,8 +55,8 @@ class Graph():
                     visited[i] = True
 
     def dfs_search(self):
-        for v in range(self.num_vertices):
-            self.visited = [False] * self.num_vertices
+        for v in range(self.vertices):
+            self.visited = [False] * self.vertices
             self.count = 0 # initialize connected vertex count
             print "DFS from {}:".format(v),
             self.dfs(v)
@@ -64,8 +64,8 @@ class Graph():
             print "Vertices connected {}".format(v, self.count)
 
     def dfs_search_2(self):
-        self.visited = [False] * self.num_vertices
-        for u in range(self.num_vertices):
+        self.visited = [False] * self.vertices
+        for u in range(self.vertices):
             if not self.visited[u]:
                 self.dfs(u)
 
@@ -77,14 +77,35 @@ class Graph():
             if not self.visited[v]:
                 self.dfs(v)
 
+    def topo_sort_dfs(self, u, tlist):
+        self.visited[u] = True
+        for v in self.adj[u]:
+            if not self.visited[v]:
+                self.topo_sort_dfs(v, tlist)
+        tlist.insert(0, u)
+
     def topo_sort(self):
-        pass
+        """
+        Topological sort is a sorting of vertices in a DAG
+        such that if there is an edge from u -> v, then u
+        appears before v in the sorted order.
+
+        A modified dfs of the graph where the vertices are stored
+        in a stack instead of printing them accomplishes them
+        Do a dfs through all the vertices and add the vertices to
+        the front of a stack based on finishing times.
+        """
+        tlist = []
+        self.visited = [False] * self.vertices
+        for u in range(self.vertices):
+            if not self.visited[u]:
+                self.topo_sort_dfs(u, tlist)
+        return tlist
 
 
 if __name__ == '__main__':
     """
     Example Graph
-    """
     g = Graph(6)
     g.add_edge(5,0)
     g.add_edge(5,2)
@@ -96,3 +117,13 @@ if __name__ == '__main__':
     # g.dfs_search()
     g.dfs_search_2()
     # print g.bfs(5)
+    """
+    g = Graph(10)
+    g.add_edge(2, 9)
+    g.add_edge(9,4)
+    g.add_edge(4,5)
+    g.add_edge(3,4)
+    g.add_edge(3,7)
+    g.add_edge(8,7)
+    g.add_edge(7,5)
+    print g.topo_sort()
