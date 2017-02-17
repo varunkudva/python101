1b73fbfcc28c34d0500e1e4160dc6e579f83438b
Varun Kudva <varun.kudva@gmail.com>
2017-02-16 23:25:53 -0800

    tries modified

--- lib/tries.py
+++ lib/tries.py
@@ -1,75 +1,43 @@
-class tnode():
+"""
+trie module
+"""
+CHARACTER_RANGE = 26
+def char_index(c):
+    return (ord(c) - ord('a'))
+
+class Node():
     def __init__(self):
-        self.children = {}
+        self.children = [None] * CHARACTER_RANGE
         self.words = 0
         self.prefixes = 0
-
 
 class Trie():
     def __init__(self):
         # initialize trie root
-        self.root = tnode()
+        self.root = Node()
 
     def add_word(self, word):
         node = self.root
         for char in word:
             node.prefixes += 1
-            if char not in node.children:
-                node.children[char] = tnode()
-            node = node.children[char]
+            if node.children[char_index(char)] is None:
+                node.children[char_index(char)] = Node()
+            node = node.children[char_index(char)]
         node.words += 1
         node.prefixes += 1
 
     def count_prefixes(self, word):
         node = self.root
         for char in word:
-            if char in node.children:
-                node = node.children[char]
+            if node.children[char_index(char)]:
+                node = node.children[char_index(char)]
             else:
                return 0
         return node.prefixes
 
-    # def count_words2(self, node, word):
-    #     if not word:
-    #         return node.words
-    #     index = ord(word[0]) - ord('a')
-    #     if node.children[index] is None:
-    #         return 0
-    #     else:
-    #         return self.count_words2(node.children[index], word[1:])
-    #
-    # def add_word2(self):
-    #     if not word:
-    #         node.words += 1
-    #     else:
-    #         node.prefixes += 1
-    #         index = ord(word[0]) - ord('a')
-    #         if node.children[index] is None:
-    #             node.children[index].appendtnode()
-    #         self.add_word(node.children[index], word[1:])
-    #
-    # def count_prefixes2(self, node, word):
-    #     if not word:
-    #         return node.prefixes
-    #
-    #     index = ord(word[0]) - ord('a')
-    #     if node.children[index] is None:
-    #         return 0
-    #     else:
-    #         return self.count_prefixes2(node.children[index], word[1:])
-    #
-    # def count_words2(self, node, word):
-    #     if not word:
-    #         return node.words
-    #     index = ord(word[0]) - ord('a')
-    #     if node.children[index] is None:
-    #         return 0
-    #     else:
-    #         return self.count_words2(node.children[index], word[1:])
-
-
-def main():
+if __name__ == '__main__':
     my_trie = Trie()
+    print "Enter number of operations:"
     n = int(raw_input().strip())
     for a0 in xrange(n):
         op, contact = raw_input().strip().split(' ')
@@ -79,5 +47,3 @@ def main():
             print my_trie.count_prefixes(contact)
 
 
-if __name__ == '__main__':
-    main()
