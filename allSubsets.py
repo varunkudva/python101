310aa63e98d09171cf733a5e95023b9151f4ed1b
varun kudva <varun.kudva@gmail.com>
2017-02-23 23:47:34 -0800

    all subsets

---
+++ allSubsets.py
@@ -0,0 +1,11 @@
+"""
+All subsets of a given set or Power set problem
+"""
+def generate_subsets(s):
+   n = len(s)
+
+
+
+if __name__ == '__main__':
+   s = ('a', 'b', 'c', 'd')
+   generate_subsets(s)
