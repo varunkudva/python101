24eef2a74ca7f0a4cc55e5c74607c7cb5c2f1727
Varun Kudva <varun.kudva@gmail.com>
2017-03-03 23:35:09 -0800

    update

--- Dynamic programming/balanceParenthesis.py
+++ Dynamic programming/balanceParenthesis.py
@@ -1,20 +1,41 @@
 """
 Given n pairs of parentheses, find all possible balanced combinations
+Solution:
 """
-def construct(n, open, close, res=""):
-    # base case and error cases
-    if open < close:
+
+
+def add_parenthesis_2(n, left=0, right=0, res=""):
+    # base and error cases
+    if left < right:
         return
-    if open == n and close == n:
+    if left == n and right == n:
         print res
     else:
-        if open < n:
-            open += 1
+        if left < n:
+            left += 1
             res += '('
-            construct(n, open, close, res)
-        if close < open:
+            add_parenthesis_2(n, left, right, res)
+        if right < left:
+            right += 1
             res += ')'
-            close += 1
-            construct(n, open, close, res)
+            add_parenthesis_2(n, left, right, res)
 
-construct(2, 0, 0)
+
+def add_parenthesis(left_rem, right_rem, res=""):
+    if left_rem < 0 or right_rem < left_rem:
+        return
+
+    if left_rem == 0 and right_rem == 0:
+        print res
+    else:
+        if left_rem:
+            res += '('
+            add_parenthesis(left_rem - 1, right_rem, res)
+
+        if right_rem:
+            res += ')'
+            add_parenthesis(left_rem, right_rem - 1, res)
+
+
+# add_parenthesis(2, 2)
+add_parenthesis_2(2)
