bf17ab796a9f853b5a718923756aa7b7c51aed68
varun kudva <varun.kudva@gmail.com>
2017-03-22 23:47:11 -0700

    updating parenthesis

--- Dynamic programming/balanceParenthesis.py
+++ Dynamic programming/balanceParenthesis.py
@@ -1,13 +1,17 @@
 """
 Given n pairs of parentheses, find all possible balanced combinations
 Solution:
+1. Start with clean slate and add left parenthesis.
+2. Add right parenthesis only if there are more left parenthesis than right.
+3. Stop if left count == n or right count > left count.
 """
 
 
 def add_parenthesis_2(n, left, right, idx, res):
     # base and error cases
-    if left < right:
+    if left > n or right > left:
         return
+
     if left == n and right == n:
         print "".join(res)
     else:
