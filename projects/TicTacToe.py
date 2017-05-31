baafd07d6bfe9a5ec5a44f7157f92205923b448d
Varun Kudva <varun.kudva@gmail.com>
2017-05-30 17:02:47 -0700

    python 3 compatibility

--- projects/TicTacToe.py
+++ projects/TicTacToe.py
@@ -1,6 +1,8 @@
 """
 This is a simple tic-tac-toe terminal game with 2 players.
 """
+from __future__ import print_function
+
 from tabulate import tabulate
 
 EMPTY_CELL = ''
@@ -27,7 +29,7 @@ class TicTacToe(object):
         self.board = [[''] * 3 for _ in range(3)]
 
     def display_board(self):
-        print tabulate(self.board, tablefmt="grid")
+        print(tabulate(self.board, tablefmt="grid"))
 
     def player_swap(self, player_num):
         if player_num == 1:
@@ -43,6 +45,7 @@ class TicTacToe(object):
                 return False
         return True
 
+    @property
     def game_over(self):
         over = False
         winner_icon = None
@@ -74,12 +77,12 @@ class TicTacToe(object):
                         over = False
 
         if over:
-            for key, player in self.players_dict.iteritems():
+            for key, player in self.players_dict.items():
                 if player.icon == winner_icon:
-                    print "Aaaaaand the WINNER is......, {}".format(player.name)
+                    print("Aaaaaand the WINNER is......, {}".format(player.name))
         else:
             if self.is_board_full():
-                print "OhOh! Not again. Its a draw!"
+                print("OhOh! Not again. Its a draw!")
                 over = True
 
         return over
@@ -92,43 +95,43 @@ class TicTacToe(object):
             while True:
                 # retry exception while loop
                 try:
-                    print "{}'s move. Enter position: ".format(self.players_dict[player_num].name)
-                    r, c = map(int, raw_input().strip().split())
-                    if r < 0 or r > 2: raise
-                    if c < 0 or c > 2: raise
+                    print ("{}'s move. Enter position: ".format(self.players_dict[player_num].name))
+                    r, c = map(int, input().strip().split())
+                    if r < 0 or r > 2: raise Exception
+                    if c < 0 or c > 2: raise Exception
                 except:
-                    print "Invalid position. Try again!"
+                    print ("Invalid position. Try again!")
                     continue
                 break
 
             if self.board[r][c]:
-                print "Position already occupied."
+                print ("Position already occupied.")
             else:
                 self.board[r][c] = self.players_dict[player_num].icon
                 self.display_board()
-                if self.game_over():
+                if self.game_over:
                     break
                 player_num = self.player_swap(player_num)
 
 
     def start(self):
         " Player initialization and begin of game loop"
-        print "Welcome to TIC TAC TOE"
+        print ("Welcome to TIC TAC TOE")
         player_num = 1
         icon = None
 
         while player_num <= 2:
-            name = raw_input("Enter player{} name: ".format(player_num))
+            name = input('Enter player{} name: '.format(player_num))
             if player_num == 1:
                 while icon != 'X' and icon != 'O':
-                    icon = raw_input("Please choose your icon: X or O: ")
+                    icon = input("Please choose your icon: X or O: ")
             else:
                 icon = 'X' if self.players_dict[1].icon == 'O' else 'O'
 
             self.players_dict[player_num] = Players(name, icon)
             player_num += 1
 
-        print "Let the Games begin"
+        print ("Let the Games begin")
         self.game_loop()
 
 
